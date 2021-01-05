cac9f3bf6c31b1cc390616742543e053d230a25b
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-05 00:11:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,16 @@ require 'pry'
 
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   final_results =  data.each_with_object({}) do |(key, value), final_array|
+=======
+  data.each_with_object({}) do |(key, value), final_array|
+>>>>>>> 32c461cc5d1418c2b8e2ca5595e899cbe75768c6
     
     #binding.pry
      value.each do |inner_key, names|
     names.each do |name|
+<<<<<<< HEAD
       #binding.pry 
       
         if !final_array[name]
@@ -25,4 +30,14 @@ def nyc_pigeon_organizer(data)
     end 
   end 
   #binding.pry
+=======
+      binding.pry 
+      
+      
+      end 
+    end 
+    final_array
+  end 
+  binding.pry
+>>>>>>> 32c461cc5d1418c2b8e2ca5595e899cbe75768c6
 end
